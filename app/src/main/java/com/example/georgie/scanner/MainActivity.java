d6ee6d894074c74a5db02ba259ac0ab7b1522093
georgeinw <georgeinw@hotmail.com>
2016-11-21 22:38:53 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/georgie/scanner/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.georgie.scanner;
+
+import android.app.AlertDialog;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler,MessageDialogFragment.MessageDialogListener {
+    private ZXingScannerView mScannerView;
+    private AlertDialog alert1;
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        setContentView(R.layout.activity_main);
+        setupToolbar();
+
+        ViewGroup contentFrame = (ViewGroup) findViewById(R.id.content_frame);
+        mScannerView = new ZXingScannerView(this);
+        contentFrame.addView(mScannerView);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+        closeMessageDialog();
+    }
+    @Override
+    public void handleResult(Result rawResult) {
+        //Toast.makeText(this, "Contents = " + rawResult.getText(), Toast.LENGTH_SHORT).show();
+
+        // Note:
+        // * Wait 2 seconds to resume the preview.
+        // * On older devices continuously stopping and resuming camera preview can result in freezing the app.
+        // * I don't know why this is the case but I don't have the time to figure out.
+        /*final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mScannerView.resumeCameraPreview(MainActivity.this);
+            }
+        },5000);*/
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+            r.play();
+        } catch (Exception e) {}
+        showMessageDialog(rawResult.getText());
+
+    }
+    public void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    public void showMessageDialog(String message) {
+        DialogFragment fragment = MessageDialogFragment.newInstance("Scan Results", message, this);
+        fragment.show(getSupportFragmentManager(), "scan_results");
+    }
+
+    public void closeMessageDialog() {
+        closeDialog("scan_results");
+    }
+
+    public void closeDialog(String dialogName) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        DialogFragment fragment = (DialogFragment) fragmentManager.findFragmentByTag(dialogName);
+        if(fragment != null) {
+            fragment.dismiss();
+        }
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        // Resume the camera
+        mScannerView.resumeCameraPreview(this);
+    }
+
+}
